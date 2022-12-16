8b6a8dd2f63dd1270b4492c126436eb55bd2af2a
LKmXr <113763590+LKmXr@users.noreply.github.com>
2022-12-16 14:36:15 +0000

    Make CSS Code Can Be Render In Any Browser

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const ip = require('ip');
 
 const app = express();
 
@@ -11,7 +10,4 @@ app.use(express.static('public'));
 
 app.listen(5500, () => {
   console.log('Server started!');
-  console.log('Server avaliable at:');
-  console.log('http://127.0.0.1:5500/');
-  console.log('http://' + ip.address() + ':5500/');
 });
